803530c3c39f361fca7e9b74fd4992f17200b6aa
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-06 16:51:11 -0300

    Update sistema_passagem_aerea6.21.c

--- sistema_passagem_aerea6.21.c
+++ sistema_passagem_aerea6.21.c
@@ -1,6 +1,24 @@
 /*
-    Arrays:
-    Autor: Pedro Filho,06/12/2020.
+   Arrays: Seu programa deverá exibir o seguinte menu de alternativas:  Favor
+   digitar 1 para “primeira classe”  Favor digitar 2 para “econômica”
+   Se a pessoa digitar 1, então seu programa deverá designar um assento na
+   primeira classe (assentos de 1  a 5). Se a pessoa digitar 2, então seu
+   programa deverá  designar um assento na classe econômica (assentos 6 a  10).
+   Seu programa deverá, então, imprimir um bilhete  de embarque que indique
+   o número doassento da pessoa  e a seção a que esse assento pertence,
+   primeira classe ou  classe econômica.  Use um array de subscrito único
+   para representar o quadro de assentos do avião. Inicialize todos os
+   elementos  do array em 0 para indicar que todos os assentos estão  vazios.
+   À medida que cada assento é designado, defina o  elemento correspondente
+   do array como 1, para indicar  que o assento não está mais disponível.
+   Seu programa, naturalmente, nunca deverá designar  um assento que já
+   foi reservado a outra pessoa. Quando a primeira classe estiver cheia,
+   seu programa deverá  perguntar à pessoa se ela aceita ser colocada na
+   classe  econômica (e vice-versa). Se a resposta for sim, faça
+   a  designação de assentos apropriada. Se a resposta for não,  imprima
+   a mensagem “O próximo voo sairá em 3  horas.”
+   Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 202). Edição do Kindle.
+   Autor: Pedro Filho,06/12/2020.
 */
 
 //bibliotecas
