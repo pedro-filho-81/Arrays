bc1d56a39404f50bea96cd9498f1ae63e04b96a5
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-06 10:32:27 -0300

    Create aula_vetor_media_mediana_moda.c

---
+++ aula_vetor_media_mediana_moda.c
@@ -0,0 +1,7 @@
+/*
+    Arrays: Figura 6.16: fig06_16.c  2 Esse programa introduz o tópico da
+    análise de dados de pesquisa.  3 Ele calcula a média, a mediana
+    e a moda dos dados.
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 181). Edição do Kindle.
+    Autor: Pedro Filho, /12/2020.
+*/
